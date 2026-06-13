7d3e1bb13dec8f3b6dde4e25a96d5abd9c62f80d
juan rubiano <juandiegorubilam@gmail.com>
2026-06-12 20:28:46 -0500

    Configurar PostgreSQL Supabase

--- barberia/settings.py
+++ barberia/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'barberia.wsgi.application'
 # DATABASE
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'postgres'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'TU_CONTRASEÑA'),
+        'HOST': os.environ.get('DB_HOST', 'db.szwuzeznhozmaztllsnz.supabase.co'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
