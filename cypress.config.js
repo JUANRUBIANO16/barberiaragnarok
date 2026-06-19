a38986625f2a9da1a0cc0420c12b90c01c5fdd7f
juan rubiano <juandiegorubilam@gmail.com>
2026-06-18 19:12:12 -0500

    testing  con cyprees

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,4 @@
-const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: "https://barberiaragnarok.onrender.com",
-    specPattern: "cypress/e2e/**/*.cy.js",
-    supportFile: "cypress/support/e2e.js",
-  },
-});
+e2e: {
+  baseUrl: "https://barberiaragnarok.onrender.com"
+}
