b40c1ebde43eaabf598afdca3e11d9784ede0825
juan rubiano <juandiegorubilam@gmail.com>
2026-06-20 18:16:51 -0500

    html loguin corregido pagina barbers

--- barberia/settings.py
+++ barberia/settings.py
@@ -136,13 +136,16 @@ USE_TZ = True
 
 # =========================
 # EMAIL (SENDGRID API - CORRECTO PARA RENDER)
-# =========================
-EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
+# =========================EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
+EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = "apikey"
+EMAIL_HOST_PASSWORD = os.getenv("SENDGRID_API_KEY")
 
 DEFAULT_FROM_EMAIL = "ragnarockbarber@gmail.com"
-
 # =========================
 # TWILIO
 # =========================
