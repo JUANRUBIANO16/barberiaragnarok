3f82c9fcc91d717b3491b92e8a08f74a947cba8c
juan rubiano <juandiegorubilam@gmail.com>
2026-06-20 12:32:55 -0500

    html loguin corregido pagina barbers

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,15 +1,19 @@
-describe('Login', () => {
+describe('Login sistema', () => {
 
   it('login correcto', () => {
 
-    cy.visit('/');
+    cy.visit('https://barberiaragnarok.onrender.com/');
 
-    cy.get('#email').type('admin@correo.com');
-    cy.get('#password').type('123456');
+    cy.get('#email').type('admin@gmail.com');
+    cy.get('#password').type('admin123');
 
     cy.get('.login-btn').click();
 
-    cy.url().should('not.include', '/login');
+    // espera redirección real
+    cy.url().should('include', '/dashboard/');
+
+    // valida que entró al sistema
+    cy.contains('Bienvenido');
   });
 
-});
+});K
