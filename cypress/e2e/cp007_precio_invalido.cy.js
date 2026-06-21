f95d7c733de7f550b036ad30a3f916bb7535fc47
juan rubiano <juandiegorubilam@gmail.com>
2026-06-21 12:04:35 -0500

    html loguin corregido pagina barbers

--- cypress/e2e/cp007_precio_invalido.cy.js
+++ cypress/e2e/cp007_precio_invalido.cy.js
@@ -2,35 +2,42 @@ describe('CP-007 - Precio inválido en servicios', () => {
 
   it('Debe bloquear creación con precio inválido', () => {
 
+    // LOGIN reutilizable
     cy.login();
 
+    // ir módulo
     cy.visit('/servicios/');
 
-    // abrir modal SIN depender de texto
+    // abrir modal de forma estable (NO texto)
     cy.get('a[href="#addEmployeeModal"]')
       .should('exist')
       .click({ force: true });
 
+    // asegurar modal abierto
     cy.get('#addEmployeeModal')
-      .should('be.visible');
-
-    cy.get('#addEmployeeModal input[name="nombre"]')
-      .type('Corte');
-
-    cy.get('#addEmployeeModal input[name="precio"]')
-      .type('-67000');
-
-    cy.get('#addEmployeeModal textarea[name="descripcion"]')
-      .type('Test');
-
-    cy.get('#addEmployeeModal button')
-      .contains('Guardar')
-      .click({ force: true });
-
-    cy.get('.alert, .custom-alert', { timeout: 10000 })
       .should('be.visible')
-      .and('contain.text', 'precio');
+      .within(() => {
 
+        cy.get('input[name="nombre"]')
+          .type('Corte');
+
+        cy.get('input[name="precio"]')
+          .clear()
+          .type('-67000');
+
+        cy.get('textarea[name="descripcion"]')
+          .type('Servicio inválido');
+
+        cy.get('button')
+          .contains('Guardar')
+          .click();
+      });
+
+    // VALIDACIÓN REAL (Django messages)
+    cy.get('.alert', { timeout: 10000 })
+      .should('exist')
+      .and('be.visible')
+      .and('contain.text', 'precio');
   });
 
 });
