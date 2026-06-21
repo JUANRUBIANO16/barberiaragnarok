a4314977edfd40745d9cf600a4e9823272142a44
juan rubiano <juandiegorubilam@gmail.com>
2026-06-20 21:22:35 -0500

    html loguin corregido pagina barbers

--- Servicios/urls.py
+++ Servicios/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.lista_servicios, name='listar_servicios'),
    
-    path('crear/', views.Crear_servicio, name='crear_servicios'),
+    path('crear/', views.crear_servicio, name='crear_servicios'),
     path('edit/<int:id>/', views.servicio_edit, name='editar_servicios'),
     path('delete/<int:id>/', views.servicio_delete, name='eliminar_servicios'),
 ]
